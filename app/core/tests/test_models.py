a383e72a2add3c0024efa2c3699ea18eca6e849e
binaerverkehr <binaerverkehr@gmail.com>
2024-05-29 11:24:22 +0200

    Added create superuser test and feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -38,3 +38,14 @@ class ModelTests(TestCase):
         """Test creating user without email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'sample123')
+
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
