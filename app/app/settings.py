1cc73f4d0bf157938367e50ce18a20ee7daffcec
binaerverkehr <binaerverkehr@gmail.com>
2024-05-29 14:38:40 +0200

    Created user app

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
     'core',
+    'user',
+    # Third-party
     'rest_framework',
     'drf_spectacular',
 ]
